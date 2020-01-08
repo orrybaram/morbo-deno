4ec8f0d23850f8cb08fd888bf9fa729811b101fc
orrybaram <orrybaram@gmail.com>
2020-01-07 19:00:32 -0500

    messing around

--- src/getGitBlame.ts
+++ src/getGitBlame.ts
@@ -36,34 +36,36 @@ const getGitBlame = async ({
     stdout: 'piped',
   });
 
+
   await process.status()
+  return process.close()
 
-  const decoder = new TextDecoder('utf-8');
+  // const decoder = new TextDecoder('utf-8');
 
-  const blame = decoder.decode(await process.output());
+  // const blame = decoder.decode(await process.output());
 
 
   // Hack: this parses the blame, hopefully will stay reliable
-  const splitBlame = blame.split('\n');
-  let commitData = {};
+  // const splitBlame = blame.split('\n');
+  // let commitData = {};
 
-  if (splitBlame.length > 1) {
-    const hash = splitBlame[0].split(' ')[0];
-    const authorName = splitBlame[1].replace('author ', '');
-    const authorEmail = splitBlame[2].replace('author-mail ', '');
-    const timestamp = splitBlame[3].replace('author-time ', '');
-    const summary = splitBlame[9].replace('summary ', '');
+  // if (splitBlame.length > 1) {
+  //   const hash = splitBlame[0].split(' ')[0];
+  //   const authorName = splitBlame[1].replace('author ', '');
+  //   const authorEmail = splitBlame[2].replace('author-mail ', '');
+  //   const timestamp = splitBlame[3].replace('author-time ', '');
+  //   const summary = splitBlame[9].replace('summary ', '');
 
-    commitData = {
-      hash,
-      authorName,
-      authorEmail,
-      timestamp,
-      summary,
-    };
-  }
+  //   commitData = {
+  //     hash,
+  //     authorName,
+  //     authorEmail,
+  //     timestamp,
+  //     summary,
+  //   };
+  // }
 
-  return commitData;
+  // return commitData;
 
 };
 
--- src/run.ts
+++ src/run.ts
@@ -1,39 +1,77 @@
 import { walk, readFileStr } from 'https://deno.land/std/fs/mod.ts';
-import { globToRegExp } from  'https://deno.land/std/path/mod.ts';
+import { globToRegExp } from 'https://deno.land/std/path/mod.ts';
 import { Options } from '../mod.ts';
 import getMessagesFromLine from './getMessagesFromLine.ts';
 import defaultDefinitions from './defaultDefinitions.ts';
 import getGitBlame from './getGitBlame.ts';
 import parallel from './limiter.ts';
 
-export default async function morbo(options: Options) {
-  const regExSkips = options.skip.map((glob) => globToRegExp(glob));
-  let messages = [];
+const loopFiles = async (iterable, messages, rootDir) => {
+  const { value } = await iterable.next();
 
-  for await (const {filename, info} of walk(options.rootDir, {skip: regExSkips})) {
-    if (info.isDirectory()) {
-      continue;
-    };
-
-    const file  = await readFileStr(filename)
-    const lines = file.split('\n');
-
-    lines.forEach(async (line, idx) => {
-      const lineNumber = idx + 1;
-      const newMessages = getMessagesFromLine(defaultDefinitions, line, lineNumber, filename)
-      if (newMessages.length) {
-        messages = [...messages, ...newMessages]
-      }
-    })
+  if (!value) {
+    return messages;
   }
 
-  const blames = messages.map((message) => {
-      return getGitBlame({ root: options.rootDir, lineNumber: message.lineNumber, fileName: message.fileName})
-  })
+  if (value.info.isDirectory()) {
+    return loopFiles(iterable, messages, rootDir);
+  }
 
-  const result = await parallel(blames, 10)
-  console.log(result)
+  const file = await readFileStr(value.filename);
+  const lines = file.split('\n');
 
-  // console.log(JSON.stringify(messages, null, 2));
-  return messages;
+
+  for(let idx = 0; idx < lines.length; idx++) {
+    const line = lines[idx];
+    const lineNumber = idx + 1;
+    const newMessages = getMessagesFromLine(
+      defaultDefinitions,
+      line,
+      lineNumber,
+      value.filename,
+    );
+    if (newMessages.length) {
+      messages = [...messages, ...newMessages];
+    }
+    console.log('----------1---------------')
+    await getGitBlame({ root: rootDir, lineNumber: lineNumber, fileName: value.filename})
+    console.log('----------2---------------')
+  }
+
+  return loopFiles(iterable, messages, rootDir);
+};
+
+export default async function morbo(options: Options) {
+  const regExSkips = options.skip.map(glob => globToRegExp(glob));
+  let messages = [];
+
+  const iterable = walk(options.rootDir, { skip: regExSkips });
+
+  messages = await loopFiles(iterable, messages, options.rootDir);
+
+  //   if (info.isDirectory()) {
+  //     continue;
+  //   };
+
+  //   const file  = await readFileStr(filename)
+  //   const lines = file.split('\n');
+
+  //   lines.forEach(async (line, idx) => {
+  //     const lineNumber = idx + 1;
+  //     const newMessages = getMessagesFromLine(defaultDefinitions, line, lineNumber, filename)
+  //     if (newMessages.length) {
+  //       messages = [...messages, ...newMessages]
+  //     }
+  //   })
+  // }
+
+  // const blames = messages.map((message) => {
+  //     return getGitBlame({ root: options.rootDir, lineNumber: message.lineNumber, fileName: message.fileName})
+  // })
+
+  // const result = await parallel(blames, 10)
+  // console.log(result)
+
+  console.log(JSON.stringify(messages, null, 2));
+  // return messages;
 }
