ba5ee8b4a1e114f8e51527e9ab6638d582def593
orrybaram <orrybaram@gmail.com>
2020-01-14 00:25:18 -0500

    add deps file

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export { copy, writeFileStr, walk, readFileStr } from 'https://deno.land/std@v0.28.1/fs/mod.ts';
+export { join, parse, globToRegExp, resolve } from 'https://deno.land/std@v0.28.1/path/mod.ts';
+export { parse as parseFlag} from 'https://deno.land/std@v0.28.1/flags/mod.ts'
